bd05c65ab089b45855020b48be0b050a661d4b5f
MuhummadShohag <muhummadds@gamil.com>
2021-10-01 17:23:22 +0300

    Second Chapter Practice some kind of topic

---
+++ Second_Chapter/variables.js
@@ -0,0 +1,25 @@
+/*
+    ***Variables: JavaScript variables are containers which can store data values
+                ---All JavaScript varibles must be indentified with uniques names
+                ---This unique names are called indentified
+                ---JavaScript indentifiers are case-sensitive
+*/
+
+var name = 'Muhammad Shohag Islam'
+var age = 23
+
+console.log("My name is "+name)
+console.log("I am "+age+" years old")
+
+console.log("My name is Muhammad Shohag Islam")
+console.log("I am 23 years old")
+
+/* If you use variables, you have to declear variables before console.log() or execution, 
+    it will be execute undefined 
+*/
+
+// console.log("I am a "+profession)
+// console.log("I am student of "+university)
+
+// var profession = "Student"
+// var university = "Abdullah Gul University"
---
+++ dataTypes.js
@@ -0,0 +1,18 @@
+/*
+    Data Type: the data type of a value or variable in some context is an attribute that tells what kind of data that value can have
+    JavaScript variables can hold different data types: primitive and object type data type
+    Primitive Data Type are Defined by JavaScript: Number, Boolean, Null, Undefined, string
+    Object Types are User Defined Data Type: Array, object, function
+*/
+
+Number 
+// 10,3,3.4, 12
+
+String
+'This is a text'+'This is also Text'
+
+Boolean
+true, false
+
+undefined
+null
