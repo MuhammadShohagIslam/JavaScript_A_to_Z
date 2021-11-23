07c18f512993d1fed99b701083dd003b2e4c3d05
MuhummadShohag <muhammadshohagislam.software.e@gmail.com>
2021-11-23 18:19:59 +0300

    Practice done with definition of functional programming

---
+++ Functional_Programming/functional_definition.js
@@ -0,0 +1,12 @@
+/*
+    Functional Programming: when a function act as a value, we called it is a functional programming.
+    *It is evaluted of mathmatical function and avoid changeing state as well as mutable data.
+
+    There are three terms in the functional programming
+        1. Pure Function.
+        2. First Class Function.
+        3. Higher Order Function.
+    
+    Actually JavaScript is not really functional programming, but we can use this three terms of the functional programming.
+
+*/
