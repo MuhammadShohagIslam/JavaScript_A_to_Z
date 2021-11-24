6e2df8bc9ecaa2d88ef877b72141b657d6964b12
MuhummadShohag <muhammadshohagislam.software.e@gmail.com>
2021-11-24 14:11:07 +0300

    Practice done with forEachk function

---
+++ Functional_Programming/forEach_function.js
@@ -0,0 +1,31 @@
+/*
+    forEach() method: the forEach() method calls a function for each element in an array.
+    
+    * The forEach() method is not executed for empty elements.
+    Syntax:
+        array.forEach(function(currentValue, index, arr), thisValue)
+
+*/
+const arr = [1, 3, 4, 5, 6];
+
+// let sum = 0;
+// arr.forEach(function(value, index, array){
+//     return sum += value
+//     // console.log(value, index, array);
+// })
+// console.log(sum);
+
+// manually create a forEach() methods.
+function forEach(arr, cb) {
+    for (let i = 0; i < arr.length; i++ ){
+        cb(arr[i], i, arr);
+    }
+}
+
+let sum = 0;
+
+forEach(arr, function(value, index, array){
+    console.log(value, index, array);
+    sum += value;
+})
+console.log(sum)
