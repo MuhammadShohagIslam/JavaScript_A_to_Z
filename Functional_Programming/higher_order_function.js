03837437ec5b02c98e86506375f4afd80b751f87
MuhummadShohag <muhammadshohagislam.software.e@gmail.com>
2021-11-27 13:03:01 +0300

    Practice done with Functional Programming topic like higher order and sorting

--- Functional_Programming/higher_order_function.js
+++ Functional_Programming/higher_order_function.js
@@ -5,35 +5,64 @@
 
 // we can pass function as a arguments, it is not useful function.
 function firstClass(a, b) {
-    return a + b;
+  return a + b;
+}
+
+function firstClass1(a, b, func) {
+  let c = a + b;
+  let d = a - b;
+  function sum() {
+    let m = func(10, 20);
+    return c + d + m;
   }
-  
-  function firstClass1(a, b, func) { 
-    let c = a + b;
-    let d = a - b;
-    function sum() {
-      let m = func(10, 20);
-      return c + d + m;
+  return sum;
+}
+let x = firstClass1(10, 20, firstClass2); // firstClass2 function is a argument of func paramater
+console.log(x());
+
+// we can return function from another function
+function firstClass2(a, b) {
+  return a + b;
+}
+
+function firstClass3(a, b, func) {
+  let g = a + b;
+  let h = a - b;
+  return function () {
+    // return this function from firstClass3 function
+    let m = func(15, 20);
+    return g + h + m;
+  };
+}
+let y = firstClass3(10, 40, firstClass2);
+console.log(y());
+
+// Another Example
+function greet(msg) {
+  function greeting(name) {
+    return `${msg} ${name}`;
+  }
+  return greeting;
+}
+let gm = greet("Assalamualikum(Praise and Allah's Mercy Upon You)");
+let gn = gm("Muhammad Sojib Brother");
+console.log(gn);
+
+// create power function using higher order function
+function base(baseNum) {
+  function power(powerNum) {
+    let result = 1;
+    for (let i = 1; i <= powerNum; i++) {
+      result *= baseNum;
     }
-    return sum;
+    return result;
   }
-  let x = firstClass1(10, 20, firstClass2); // firstClass2 function is a argument of func paramater
-  console.log(x());
+  return power;
+}
+let b = base(2);
+let p = b(3);
+console.log(p);
 
-  // we can return function from another function
-  function firstClass2 (a, b) {
-      return a + b;
-  }
-
-  function firstClass3 (a , b, func) {
-      let g = a + b;
-      let h = a - b;
-      return function () { // return this function from firstClass3 function
-        let m = func( 15, 20 );
-        return g + h + m;
-      }
-  }
-  let y = firstClass3(10, 40, firstClass2);
-  console.log(y());
-
-
+let b1 = base(3);
+let p1 = b1(2);
+console.log(p1);
--- Functional_Programming/others_function.js
+++ Functional_Programming/others_function.js
@@ -4,12 +4,12 @@
     The keys() method returns an Array Iterator object with the keys of an array.
 
 */
-// create from() method 
+// create from() method
 const str = "Bangladesh";
 const x = Array.from(str);
 console.log(x);
 
-// create keys() method 
+// create keys() method
 const fruits = ["Olive", "Banana", "Apple"];
 const y = fruits.keys();
 console.log(y); // return array iterator
@@ -17,10 +17,57 @@ console.log(y); // return array iterator
 // for ( let i = 0; i < fruits.length; i++){
 //     console.log(fruits[i])
 // }
-let text = '';
-for (let i of y){
-    text += i + " ";
-    // console.log(i)
+let text = "";
+for (let i of y) {
+  text += i + " ";
+  // console.log(i)
 }
 console.log(text);
 
+// create sorting over Array of Object
+const persons = [
+  {
+    name: "A",
+    age: 24,
+  },
+  {
+    name: "B",
+    age: 25,
+  },
+  {
+    name: "C",
+    age: 26,
+  },
+  {
+    name: "D",
+    age: 27,
+  },
+];
+persons.sort(function (a, b) {
+  if (a.age > b.age) {
+    return 1;
+    // return -1; // descending order
+  } else if (a.age > b.age) {
+    return -1;
+    // return 1;  // descending order
+  } else {
+    return 0;
+  }
+});
+console.log(persons);
+
+// ceate a sort() method using Array
+const arr = [1, 3, 4, 5, 6, 7, 7, 8, 1, -2, -8, 0];
+arr.sort(); // not working when comes a negative
+arr.sort(function (a, b) {
+  if (a > b) {
+    return 1;
+    // return -1;  // descending order
+  } else if (a < b) {
+    return -1;
+    // return 1; // descending order
+  } else {
+    return 0;
+  }
+});
+console.log(arr); // it is working perfectly
