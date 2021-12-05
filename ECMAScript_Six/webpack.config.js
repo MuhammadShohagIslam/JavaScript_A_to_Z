c1d06d5e6b21385bdd8ed84cceeefdc42f45487c
MuhummadShohag <muhammadshohagislam.software.e@gmail.com>
2021-12-05 19:00:57 +0300

    Add Webpack Setup For ES6 Working Purpose

---
+++ ECMAScript_Six/webpack.config.js
@@ -0,0 +1,35 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const mode = process.env.NODE_ENV === "production" ? "production" : "development"
+
+module.exports = {
+    mode: mode,
+    entry: './src/index.js',
+    output: {
+        filename: 'index.js',
+        path: path.resolve(__dirname, 'dist'),
+        clean: true
+    },
+    module : {
+        rules: [
+            {
+                test: /\.m?js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader'
+                }
+            },
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: "./public/index.html"
+        })
+    ],
+    devServer : {
+        compress: true,
+        hot: true,
+        open: true
+    }
+
+}
